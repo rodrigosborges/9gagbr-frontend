5512333105b15e948b88ffbed55294831f68a60a
Rodrigo Borges <gmrodrigoborges@gmail.com>
2019-09-09 17:52:57 -0300

    alterando css para ficar responsivo a tela de login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -15,12 +15,12 @@ export default class Feed extends React.Component {
     }
 
     _changeForm(register){
-        if(this.state.classForm == ""){
+        if(this.state.classForm === ""){
             this.setState({
                 register,
                 classForm: "transition-login-right"
             })
-        }else if(this.state.classForm == "transition-login-right"){
+        }else if(this.state.classForm === "transition-login-right"){
             this.setState({
                 register,
                 classForm: "transition-login-right transition-login-left",
@@ -41,6 +41,16 @@ export default class Feed extends React.Component {
         }
     }
 
+    resize = () => this.forceUpdate()
+
+    componentDidMount() {
+      window.addEventListener('resize', this.resize)
+    }
+    
+    componentWillUnmount() {
+      window.removeEventListener('resize', this.resize)
+    }
+
     render(){
         return (
             <div className="div-content">
@@ -53,28 +63,36 @@ export default class Feed extends React.Component {
                         </div>
                         <div className={(this.state.register === false ? "form-login-info-right" : "form-login-info-left")+" text-center"}>
                             {this.state.register === false && 
-                                <div>
-                                    <img className="mt-5" src={Logo}></img>
-                                    <div className="my-5">
-                                        <span className="form-login-info-title">Ainda não possui conta?</span>
+                                <div className={window.innerWidth < 800 ? "row mb-3" : ""}>
+                                    <div className={window.innerWidth < 800 ? "col-4 offset-2 my-auto" : ""}>
+                                        <img src={Logo}></img>
                                     </div>
-                                    <div>
-                                        <button onClick={() => {this._changeForm(true)}} className="btn btn-lg button-navbar">
-                                            Registrar
-                                        </button>
+                                    <div className={window.innerWidth < 800 ? "col-4 my-auto" : ""}>
+                                        <div className={window.innerWidth < 800 ? "mb-4 mt-4" : "my-5"}>
+                                            <span className="form-login-info-title">Não possui conta?</span>
+                                        </div>
+                                        <div  className={window.innerWidth < 800 ? "mb-4" : ""}>
+                                            <button onClick={() => {this._changeForm(true)}} className="btn btn-lg button-navbar">
+                                                Registrar
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             }
                             {this.state.register === true && 
-                                <div>
-                                    <img className="mt-5" src={Logo}></img>
-                                    <div className="my-5">
-                                        <span className="form-login-info-title">Já possui conta?</span>
+                                <div className={window.innerWidth < 800 ? "row mt-4" : ""}>
+                                    <div className={window.innerWidth < 800 ? "col-4 offset-2 my-auto" : ""}>
+                                        <img src={Logo}></img>
                                     </div>
-                                    <div>
-                                        <button onClick={() => {this._changeForm(false)}} className="btn btn-lg button-navbar">
-                                            Entrar
-                                        </button>
+                                    <div className={window.innerWidth < 800 ? "col-4 my-auto" : ""}>
+                                        <div className={window.innerWidth < 800 ? "mt-4 mt-4" : "my-5"}>
+                                            <span className="form-login-info-title">Já possui conta?</span>
+                                        </div>
+                                        <div  className={window.innerWidth < 800 ? "mt-4" : ""}>
+                                            <button onClick={() => {this._changeForm(false)}} className="btn btn-lg button-navbar">
+                                                Entrar
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             }
@@ -84,12 +102,12 @@ export default class Feed extends React.Component {
                                 <div className="row h-100">
                                     <div className="my-auto col-md-10 offset-md-1 text-center form-color">
                                         <span className="form-login-title">Entrar</span>
-                                        <div className="input-field mt-5">
+                                        <div className="input-field mt-5 mx-2">
                                             <i className="fas fa-at prefix"></i>
                                             <input id="Nome" type="text" className="validate" />
                                             <label htmlFor="Nome">E-mail</label>
                                         </div>
-                                        <div className="input-field mt-5">
+                                        <div className="input-field mt-5 mx-2">
                                             <i className="fas fa-key prefix"></i>
                                             <input id="password" type="text" className="validate" />
                                             <label htmlFor="password">Password</label>
@@ -104,17 +122,17 @@ export default class Feed extends React.Component {
                                 <div className="row h-100">
                                     <div className="my-auto col-md-10 offset-md-1 text-center form-color">
                                         <span className="form-login-title">Registrar</span>
-                                        <div className="input-field mt-5">
+                                        <div className="input-field mt-5  mx-2">
                                             <i className="fas fa-user prefix"></i>
                                             <input id="Nome" type="text" className="validate" />
                                             <label htmlFor="Nome">Nome</label>
                                         </div>
-                                        <div className="input-field mt-5">
+                                        <div className="input-field mt-5  mx-2">
                                             <i className="fas fa-at prefix"></i>
                                             <input id="email" type="text" className="validate" />
                                             <label htmlFor="email">E-mail</label>
                                         </div>
-                                        <div className="input-field mt-5">
+                                        <div className="input-field mt-5  mx-2">
                                             <i className="fas fa-key prefix"></i>
                                             <input id="password" type="text" className="validate" />
                                             <label htmlFor="password">Password</label>
